457f815947a42aba279f8972fd01ec7e7baad1b9
Tomas Rojas <tmsrjs@gmail.com>
2013-11-08 00:56:12 -0300

    extract method for generating arguments for translated route

--- lib/route_translator/translator.rb
+++ lib/route_translator/translator.rb
@@ -22,19 +22,23 @@ module RouteTranslator
 
     def self.translations_for(app, conditions, requirements, defaults, route_name, anchor, route_set, &block)
       I18n.available_locales.each do |locale|
-        new_conditions = conditions.dup
-        new_conditions[:path_info] = translate_path(conditions[:path_info], locale)
-        if new_conditions[:required_defaults] && !new_conditions[:required_defaults].include?(RouteTranslator.locale_param_key)
-          new_conditions[:required_defaults] << RouteTranslator.locale_param_key if new_conditions[:required_defaults]
-        end
-        new_defaults = defaults.merge(RouteTranslator.locale_param_key => locale.to_s)
-        new_requirements = requirements.merge(RouteTranslator.locale_param_key => locale.to_s)
-        new_route_name = translate_name(route_name, locale)
-        new_route_name = nil if new_route_name && route_set.named_routes.routes[new_route_name.to_sym] #TODO: Investigate this :(
-        block.call(app, new_conditions, new_requirements, new_defaults, new_route_name, anchor)
+        block.call(app, *translate_args(conditions, requirements, defaults, route_name, route_set, locale), anchor)
       end
       block.call(app, conditions, requirements, defaults, route_name, anchor) if RouteTranslator.config.generate_unlocalized_routes
       add_untranslated_helpers_to_controllers_and_views(route_name, route_set.named_routes.module)
+    end
+
+    def self.translate_args(conditions, requirements, defaults, route_name, route_set, locale)
+      new_conditions = conditions.dup
+      new_conditions[:path_info] = translate_path(conditions[:path_info], locale)
+      if new_conditions[:required_defaults] && !new_conditions[:required_defaults].include?(RouteTranslator.locale_param_key)
+        new_conditions[:required_defaults] << RouteTranslator.locale_param_key
+      end
+      new_defaults = defaults.merge(RouteTranslator.locale_param_key => locale.to_s)
+      new_requirements = requirements.merge(RouteTranslator.locale_param_key => locale.to_s)
+      new_route_name = translate_name(route_name, locale)
+      new_route_name = nil if new_route_name && route_set.named_routes.routes[new_route_name.to_sym] #TODO: Investigate this :(
+      [ new_conditions, new_requirements, new_defaults, new_route_name ]
     end
 
     # Translates a path and adds the locale prefix.
