3040ce4b28fe23a5930d006fb5ae42ff2054f498
Tomas Rojas <tmsrjs@gmail.com>
2013-11-07 21:59:16 -0300

    avoid rewriting methods

--- lib/route_translator/extensions/route_set.rb
+++ lib/route_translator/extensions/route_set.rb
@@ -3,9 +3,20 @@ require 'action_dispatch'
 module ActionDispatch
   module Routing
     class RouteSet
+      attr_accessor :localized
+
+      alias_method :orig_add_route, :add_route
+      def add_route(*args)
+        if @localized
+          add_localized_route(*args)
+        else
+          orig_add_route(*args)
+        end
+      end
+
       def add_localized_route(app, conditions = {}, requirements = {}, defaults = {}, as = nil, anchor = true)
         RouteTranslator::Translator.translations_for(app, conditions, requirements, defaults, as, anchor, self) do |*translated_args|
-          add_route(*translated_args)
+          orig_add_route(*translated_args)
         end
       end
     end
